b64c01dd4bc82e14942426c5e792327667234560
BrandonPu <bpu-2023395@Kinal.edu.gt>
2025-02-26 15:40:06 -0600

    se elimino el role ya que no es necesario

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import { login } from './auth.controller.js'
-import { loginValidator } from '../middlewares/validator.js';
 import { deleteFileOnError } from '../middlewares/delete-file-on-error.js';
 
 const router = Router();
 
 router.post(
     '/login',
-    loginValidator,
     deleteFileOnError,
     login
 );
